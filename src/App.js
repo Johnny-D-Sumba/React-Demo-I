d9cc60bf5f3c8b94a77df86e629f13c1692262a3
Johnny D. Sumba <jhonnydanilo177@gmail.com>
2022-07-13 19:46:55 -0500

    AppJS and AppCSS updated

--- src/App.js
+++ src/App.js
@@ -5,18 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h3>Menu</h3>
         <img src={logo} className="App-logo" alt="logo" />
+        <hr />
+        <h5>Provincias:</h5>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Nombre
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button>Agregar</button>
+        
       </header>
     </div>
   );
